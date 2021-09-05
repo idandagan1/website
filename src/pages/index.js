2182835e6a6c2d271775b587a097960b884bf809
Idan Dagan <idan@lusha.co>
2021-09-06 00:02:07 +0300

    feat: added social links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { SocialIcon } from 'react-social-icons';
 
 // styles
 const pageStyles = {
   display: 'flex',
   justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
   padding: 100,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
@@ -14,12 +17,39 @@ const linkStyles = {
   color: 'black',
 }
 
+const socialContainer = {
+  display: 'flex',
+  justifyContent: 'center',
+}
+
+const socialItem = {
+    padding: 5,
+}
+
 const Page = () => {
   return (
     <main style={pageStyles}>
       <title>Idan Dagan</title>
-      <div>
-        <h1><Link style={linkStyles} target='_blank' to="https://github.com/idandagan1">Idan Dagan</Link></h1>
+      <h1><Link style={linkStyles} target='_blank' to="https://github.com/idandagan1">Idan Dagan</Link></h1>
+      <div style={socialContainer}>
+        <div style={socialItem}>
+          <SocialIcon target="_blank" url="https://www.linkedin.com/in/idandagan1/" />
+        </div>
+        <div style={socialItem}>
+          <SocialIcon target="_blank" url="https://www.facebook.com/IdanDagan8/" />
+        </div>
+        <div style={socialItem}>
+          <SocialIcon target="_blank" url="https://stackoverflow.com/users/5732327/idan-dagan" />
+        </div>
+        <div style={socialItem}>
+          <SocialIcon target="_blank" url="https://github.com/idandagan1" />
+        </div>
+        <div style={socialItem}>
+          <SocialIcon target="_blank" url="https://twitter.com/idandagan1" />
+        </div>
+        <div style={socialItem}>
+          <SocialIcon target="_blank" url="https://medium.com/@idandagan" />
+        </div>
       </div>
     </main>
   )
